1a9dd78e0f6af5ae833ac1491605335966929eaf
sapazos <sapazos@gmail.com>
2018-08-08 08:48:39 -0300

    agrego filtros a pantalla modificar entrada

--- php/consultas/co_entradas.php
+++ php/consultas/co_entradas.php
@@ -6,10 +6,12 @@ class co_entradas
     {
 	if (!isset($where)) $where = '1=1';
         $sql = "SELECT entradas.*,
-                        destinos.descripcion as destino_desc
+                        destinos.descripcion as destino_desc,
+                        entradas_tipos.descripcion as tipo_desc
 		FROM entradas LEFT OUTER JOIN destinos ON (entradas.destino = destinos.destino)
+                LEFT OUTER JOIN entradas_tipos ON (entradas.entrada_tipo = entradas_tipos.entrada_tipo)
 		WHERE $where
-                ORDER BY numero::Int
+                ORDER BY numero::Int DESC
         ";
 	return toba::db()->consultar($sql);
     }
