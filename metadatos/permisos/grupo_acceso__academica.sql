b22939d10858e9be944b24a595e342197ccbbded
sapazos <sapazos@gmail.com>
2019-04-26 10:38:12 -0300

    permisos para ver reportes

--- metadatos/permisos/grupo_acceso__academica.sql
+++ metadatos/permisos/grupo_acceso__academica.sql
@@ -103,4 +103,16 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	NULL, --item_id
 	'280000174'  --item
 );
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'docu', --proyecto
+	'academica', --usuario_grupo_acc
+	NULL, --item_id
+	'280000229'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'docu', --proyecto
+	'academica', --usuario_grupo_acc
+	NULL, --item_id
+	'280000230'  --item
+);
 --- FIN Grupo de desarrollo 280
